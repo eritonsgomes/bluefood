63e02dae753f6ced363b875a5593d63f8d2b2322
Eriton Santos <eritonsg@gmail.com>
2024-09-30 14:19:58 -0300

    Projeto criado

---
+++ README.md
@@ -0,0 +1,5 @@
+## BlueFood
+
+### Aplicação Web de Pedios de Comida
+
+Este projeto foi desenvolvido durante a realização do Curso Master Developer da Empresa Softblue Cursos Online.
---
+++ src/test/java/br/com/softblue/bluefood/bluefood/BluefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softblue.bluefood.bluefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluefoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
